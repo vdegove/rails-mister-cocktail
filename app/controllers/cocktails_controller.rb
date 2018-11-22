8a4a8310206336700a446ae29add0bddbee0c182
Vincent Degove <vincent.degove@muage.net>
2018-11-22 14:40:18 +0100

    Add simple CRUD actions and views to cocktails

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,42 @@
+class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    Cocktail.create(cocktail_params)
+    redirect_to cocktails_path
+  end
+
+  def edit
+  end
+
+  def update
+    @cocktail.update(cocktail_params)
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def destroy
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+end
